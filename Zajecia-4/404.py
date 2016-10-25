5b877fefa8f6f2c9eb361ee5f59e3ce209450714
Marcin Żołubowski <marcin.zolubowski@student.uj.edu.pl>
2016-10-25 13:00:17 +0200

    Add files via upload

---
+++ Zajecia-4/404.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+	a = 0
+	b = 1
+	if (n == 0): 
+		return 0
+	for i in range(1, n):
+		b += a
+		a = b-a
+			
+	return b
+
+x = raw_input("Podaj n ciagu fibonacciego:")
+print fibonacci(int(x))
