289e60259953a59d4da81be6b01351f5cb42ea10
Marcin Żołubowski <marcin.zolubowski@student.uj.edu.pl>
2016-11-01 22:28:36 +0100

    Update 404.py

--- Zajecia-4/404.py
+++ Zajecia-4/404.py
@@ -1,15 +1,8 @@
-#kompilacja z python3
+def factorial(n):
+	a = 1;
+	for i in range(int(n), 1, -1):
+		a *= i
+	return a
 
-def fibonacci(n):
-	a = 0
-	b = 1
-	if (n == 0): 
-		return 0
-	for i in range(1, n):
-		b += a
-		a = b-a
-			
-	return b
-
-x = input("Podaj n-ty wyraz ciagu fibonacciego:")
-print(fibonacci(int(x)))
+x = input("Podaj n-ty wyraz silni do obliczenia:")
+print(factorial(x))
